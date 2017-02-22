e7f9d9b11afc79128236fd0bb66dbf34731a16f2
Nat <Nat@wifi-roaming-128-4-213-254.host.udel.edu>
2017-02-22 10:33:48 -0500

    commit 2

--- src/lab1/main.java
+++ src/lab1/main.java
@@ -1,11 +1,11 @@
 package lab1;
-
+// Natalie Russo, Rebecca Schwartz, Mike Rawding
 import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 	Scanner input = new Scanner(System.in);
 	
 	double monthlysaving;
@@ -17,7 +17,7 @@ public class main {
 	double requiredincome;
 	double monthlySSI;
 	
-	
+	//these are the variables that will be obtained by the user and calculated
 	
 	System.out.println("Enter years you will work? ");
 	yearstowork = input.nextDouble();
@@ -28,6 +28,7 @@ public class main {
 	
 	returnwhileworking =input.nextDouble();
 	}while (returnwhileworking < 0 || returnwhileworking > 20);
+	// do while loop is used in order to ensure a valid entry is input into the system
 	
 	System.out.println("How long do you want to draw?");
 	yearsretired = input.nextDouble();
@@ -37,6 +38,8 @@ public class main {
 	
 	annualreturn = input.nextDouble();
 	}while (annualreturn < 0 || annualreturn > 3);
+	
+	// do while loop is used in order to ensure a valid entry is input into the system, a value from 0 to 3
 	
 	System.out.println("What is your required income?");
 	requiredincome = input.nextDouble();
@@ -48,16 +51,18 @@ public class main {
 	double n = (yearsretired*12);
 	
 	moneyneeded = (requiredincome - monthlySSI)*((1-(Math.pow((1+r),-n)))/r);
+	// formula for Present Value of Annuity
 	
 	double r1 = (returnwhileworking/100)/12;
 	double n1 = (yearstowork*12);
 	
 	monthlysaving = (moneyneeded*r1)/(Math.pow((1+r1), n1)-1);
+	//formula to calculate Future Value of Annuity
 	
 	System.out.printf("Money needed to be saved: $%10.2f\n", moneyneeded);
 	System.out.printf("Amount needed to be saved monthly: $%10.2f", monthlysaving);
 	input.close();
-	
+	//system gives user total money needed to be saved and how much to save each month
 	}
 	
 }
